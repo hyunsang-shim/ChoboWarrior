0e667f44ac555862957215744c4f98ceb3de811f
hyunsang-shim <shimhyunsang.dev@gmail.com>
2023-04-02 11:55:58 +0900

    수련하기 팝업 작업 시작
    시작.

---
+++ Assets/Scripts/popup_Training.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class popup_Training : MonoBehaviour
+{
+    public Slider Progress;
+    public Button btnAbort;
+    public GameObject Result_Success;
+    public GameObject Result_GreatSuccess;
+    public GameObject Result_Fail;
+
+    public float TrainingTime;
+
+}
